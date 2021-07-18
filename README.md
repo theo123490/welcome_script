5e9d1fac92d8060dd5824fdc728a787f3da050c7
Theodore Gautama Chandra <theoengphys@gmail.com>
2021-07-18 23:31:39 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Startup Script welcome message script
+Art by lunatic 42
+
+https://www.asciiart.eu/video-games/pokemon
---
+++ startup.sh
@@ -0,0 +1,17 @@
+printf "             \`;,;.;,;.;.'                     | Date : " ; date '+%Y-%m-%d'
+printf "              ..:;:;::;:                      | Time : "; date '+%H:%M:%S'
+printf "        ..--''' '' ' ' '''--.                 | User : "; whoami
+printf "      /' .   .'        '.   .\`\\               | Uptime : "; uptime
+printf "     | /    /            \\   '.|              |    \n"
+printf "     | |   :             :    :|              |     \n"
+printf "   .'| |   :             :    :|              |     \n"
+printf " ,: /\\ \\.._\\ __..===..__/_../ /\`.             |     \n"
+printf "|'' |  :.|  \`'          \`'  |.'  ::.          |     \n"
+printf "|   |  ''|    :'';          | ,  \`''\\         |     \n"
+printf "|.:  \\/  | /'-.\`'   ':'.-'\\ |  \\,   |         |     \n"
+printf "| '  /  /  | / |...   | \\ |  |  |';'|         |     \n"
+printf " \\ _ |:.|  |_\\_|\`.'   |_/_|  |.:| _ |         |     \n"
+printf "/,.,.|' \\__       . .      __/ '|.,.,\\        |     \n"
+printf "     | ':\`.\`----._____.---'.'   |             |     \n"
+printf "      \\   \`:\"\"\"-------'\"\"' |    |             | session starting\n"
+printf "       ',-,-',             .'-=,=,            | don't do stupid things\n"
